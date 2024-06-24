2b1f8cfc208017c545b45ea68d1db068d727f988
LucaModric <modric@163.com>
2024-06-24 18:22:03 +0800

    增加自动生成PDF对应的MD文件的功能

--- README.md
+++ README.md
@@ -17,14 +17,28 @@ docsify好像没法自动读取文件夹目录结构并且展示在页面上,需
 ```txt
 [config]
 # docsify根目录
-base_dir=D:\MyData\Data\Docsify\docs
+base_dir=C:\Users\Administrator\Desktop\test\demo
 # 忽略以“_”,"."开头的文件，如果要添加新文件，用“|”分隔
 ignore_start_with=_|.
 # 只读取".md"格式问价，如果添加新格式，用“|”分隔
 show_file=.md
 # 要忽略的文件名，要添加新文件，用“|”分隔
 ignore_file_name=README
+# 是否展示文件的后缀名，空值默认不展示，填上任意字符表示展示，比如show_extension=1
+show_extension=
+#不展示的文件夹（文件下所有文件都不展示），多个文件夹用“|”分隔
+ignore_dir_name=秘密
 
+# pdf相关配置
+# 为pdf生成对应的md文件：0不生成(没有pdf文件或者不需要)；1部分生成,配合updatedPdfFiles使用可只更新updatedPdfFiles配置的pdf,当你修改了某些pdf的md,不希望覆盖可使用；
+# 2全部生成，会覆盖你之前所有pdf的md，配合ignore_pdf_filename使用可忽略指定的pdf不更新；
+pdf_write_mode=0
+# 需要更新的pdf列表，多个文件用“|”分隔，如 python基础.pdf|java基础.pdf，搭配pdf_write_mode=1使用
+updated_pdf_files=python基础.pdf|java基础.pdf
+# 不需要展示的pdf文件目录，比如"秘密"和""简历""目录里面的pdf，不想展示，多个目录用“|”，如  秘密|简历
+ignore_pdf_dirname=秘密|简历
+# 不需要展示的pdf文件，多个文件用“|”分割，如 个人简历.pdf|征信查询.pdf，搭配pdf_write_mode=2使用
+ignore_pdf_filename=个人简历.pdf|征信查询.pdf
 
 [outFile]
 # 想要在几级目录生成文件,默认"-1"表示所有文件夹生成,"0"表示在根目录生成,可以配合侧边栏折叠插件使用
@@ -107,12 +121,11 @@ docs
 ## 图片
 
 在根目录情况:
-
-![img0.png](https://github.com/LucaModric/docsify-auto-sidebar-gen/blob/master/image/img0.png)
+![img0.png](image/img0.png)
 
 点击PLC之后
 
-![img4.png](https://github.com/LucaModric/docsify-auto-sidebar-gen/blob/master/image/img4.png)
+![img4.png](image/img4.png)
 
 这种方式生成的结构,点击新文件夹会刷新界面,也可以接受
 
@@ -184,8 +197,47 @@ docs
 
 ## 图片
 
-![img.png](https://github.com/LucaModric/docsify-auto-sidebar-gen/blob/master/image/img.png)
+![img.png](image/img.png)
 
 配合侧边栏折叠插件:https://github.com/iPeng6/docsify-sidebar-collapse
 
-![img2.png](https://github.com/LucaModric/docsify-auto-sidebar-gen/blob/master/image/img2.png)
+![img2.png](image/img2.png)
+
+
+## PDF文件自动转对应的MD
+建议文件不要太大，比如几百页的书籍，就github那个网速，打开得多慢。这里推荐一个把网页保存为PDF的超便利方式：打印另存为PDF,以edge浏览器举例
+，右键点打印或者快捷键 ctrl +p 即可把网页所选内容另存为pdf,可设置缩放比例，纸张大小等。如果想编辑PDF,可以下载相关的软件。
+![另存为PDF](image/img5.gif)
+
+###  PDF文件生成对应的MD文件前目录
+![另存为PDF](image/img6.gif)
+
+###  PDF文件生成对应的MD文件后目录
+![另存为PDF](image/img7.gif)
+### 页面效果
+隐藏侧边栏，点侧边栏左下角三条横杆那个按钮，还有不设置右侧目录栏，pdf的阅读体验更好。
+
+![另存为PDF](image/img8.gif)
+以上PDF是内嵌在页面展示的，如果要把PDF设置成默认全屏展示可以在index.html覆盖pdf内嵌样式，加上下面css代码。
+```html
+ <style>
+        /* 全屏展示 PDF,如果不需要全屏展示，把整个style标签去掉 */
+        .pdfobject-container {
+            position: fixed;
+            left: 0;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            height: 100%;
+            width: 100%;
+            background: transparent;
+            z-index: 100;
+        }
+
+        .pdfobject {
+            border: none;
+            height: 100%;
+            width: 100%;
+        }
+</style>
+``` 
--- buildSidebar.py
+++ buildSidebar.py
@@ -7,22 +7,38 @@ base_dir = None
 start_with = None
 show_file = None
 ignore_file_name = None
+show_extension = None
+ignore_dir_name = None
+ignore_pdf_dirname = None
+ignore_pdf_filename = None
+updated_pdf_files = None
+pdf_write_mode = None
 
 out_file_list = []
 create_depth = -1
 
 
 def read_config():
-    global base_dir, show_file, start_with, ignore_file_name, ReadmeFile, _sidebarFile, out_file_list, create_depth
+    global base_dir, show_file, start_with, ignore_file_name, out_file_list, create_depth, show_extension, ignore_dir_name
+    global ignore_pdf_dirname, ignore_pdf_filename, updated_pdf_files, pdf_write_mode
     # exe 默认是在盘的local/temp，此时就拿不到相对路径下的config.ini
     # sys.argv解决该bug
     cf = ConfigParser()
     print("程序执行的当前路径:   " + split(argv[0])[0])
+    # cf.read("config.ini", encoding='utf-8') 作者原来的写法
     cf.read(split(argv[0])[0] + "\config.txt", encoding='utf-8')
     base_dir = cf.get("config", "base_dir")
+    print("项目的根路径base_dir:   " + base_dir)
     start_with = cf.get("config", "ignore_start_with").split("|")
     show_file = cf.get("config", "show_file").split('|')
     ignore_file_name = cf.get("config", "ignore_file_name").split("|")
+    show_extension = bool(cf.get("config", "show_extension"))
+    ignore_dir_name = cf.get("config", "ignore_dir_name").split("|")
+
+    updated_pdf_files = cf.get("config", "updated_pdf_files").split("|")
+    ignore_pdf_dirname = cf.get("config", "ignore_pdf_dirname").split("|")
+    ignore_pdf_filename = cf.get("config", "ignore_pdf_filename").split("|")
+    pdf_write_mode = int(cf.get("config", "pdf_write_mode"))
 
     out_file_list = cf.get("outFile", "eachFile").split("|")
     create_depth = int(cf.get("outFile", "create_depth"))
@@ -49,7 +65,7 @@ def check_file_name_satified(file_path):
     """
     file_name_with_extension = basename(file_path)
     file_name = splitext(file_name_with_extension)[0]
-    if file_name[0] in start_with or file_name in ignore_file_name:
+    if file_name[0] in start_with or file_name in ignore_file_name or file_name in ignore_dir_name:
         return False
     return True
 
@@ -86,15 +102,21 @@ def save_structure(root_dir, base_dir=base_dir, depth=0):
     for file in files:
         if check_file_name_satified(join(root, file)):
             if check_file_extension(file):
-                subdir_structure += "  " + "- [" + file + "](" + relpath(join(root, file),
-                                                                         base_dir) + ')\n'
+                if show_extension:
+                    fileName = file
+                else:
+                    fileName = splitext(file)[0]
+                subdir_structure += "  " + "- [" + fileName + "](" + relpath(join(root, file), base_dir) + ')\n'
 
     for subdir in dirs:
         subdir_path = join(root, subdir)
         if check_file_name_satified(subdir_path):
             next_struct = save_structure(subdir_path, base_dir, depth + 1)
+            # 子目录缩进。最后一项不缩进，是因为同级的目录得保持一致缩进（比如a和b同级，a下有a1,a2;a1,a2遍历完后开始遍历b，
+            # 如果a2后面还缩进换行，那么此时b就和a2处于同一个缩进了，这样就不对了。
             next_struct = next_struct[:-1] if next_struct.endswith("\n") else next_struct
             next_struct = next_struct.replace("\n", "\n  ") + "\n"
+            # 同级目录与文件保持相同缩进
             subdir_structure += "  " + next_struct
 
     back_struct = subdir_structure
@@ -119,7 +141,39 @@ def save_structure(root_dir, base_dir=base_dir, depth=0):
     return back_struct
 
 
+def changePdfToMd(root_dir, base_dir=base_dir):
+    if (pdf_write_mode == 0):
+        return
+    for item in listdir(root_dir):
+        filepath = join(root_dir, item)
+        if isdir(filepath):
+            if item in ignore_pdf_dirname:
+                continue
+            else:
+                changePdfToMd(filepath, base_dir)
+        else:
+            file_name, file_extension = splitext(item)
+            if ".pdf" == file_extension.lower():
+                if pdf_write_mode == 1:
+                    if item in updated_pdf_files:
+                        writePdf(base_dir, file_name, filepath, root_dir)
+                else:
+                    if item not in ignore_pdf_filename:
+                        writePdf(base_dir, file_name, filepath, root_dir)
+
+
+def writePdf(base_dir, file_name, filepath, root_dir):
+    # md的文件名不能有空格，否则显示异常，这里重命名文件
+    filepath_md = join(root_dir, file_name.replace(" ", "_") + ".md")
+    content = "### " + file_name.replace(" ", "_") + "\n";
+    content += "```pdf\n" + "     " + relpath(filepath, base_dir).replace("\\",
+                                                                          "/") + "\n" + "```";
+    with open(filepath_md, 'w', encoding="utf-8") as f:
+        f.write(content)
+    print(f'{file_name}.pdf 成功生成对应的MD!')
+
 if __name__ == "__main__":
     read_config()
+    changePdfToMd(base_dir, base_dir)
     save_structure(base_dir, base_dir, 0)
-    input()
+    input("按任意键结束程序")
